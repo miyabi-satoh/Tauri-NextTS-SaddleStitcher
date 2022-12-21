0ed69f90be45fe89e6796b1c995ac2a6458bbcc3
miyabi-satoh <miyabi.satoh@gmail.com>
2022-12-22 02:18:40 +0900

    新規作成

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,13 @@
 import { getVersion } from "@tauri-apps/api/app";
-import { BaseDirectory, readTextFile, writeTextFile } from "@tauri-apps/api/fs";
+import {
+  BaseDirectory,
+  createDir,
+  exists,
+  readDir,
+  readTextFile,
+  writeTextFile,
+} from "@tauri-apps/api/fs";
+import { appConfigDir } from "@tauri-apps/api/path";
 
 const CONF_FILENAME = "saddlestitcher.conf.json";
 const CONF_OPTION = { dir: BaseDirectory.AppConfig };
@@ -7,22 +15,37 @@ const CONF_OPTION = { dir: BaseDirectory.AppConfig };
 export interface Config {
   version: string;
   debug: boolean;
-  python: string;
 }
 
-export async function readConfig(): Promise<Config> {
-  const contents = await readTextFile(CONF_FILENAME, CONF_OPTION);
-  if (contents) {
-    return JSON.parse(contents) as Config;
-  }
+export async function getConfigDefault(): Promise<Config> {
   return {
     version: await getVersion(),
     debug: false,
-    python: "",
-  } as Config;
+  };
+}
+
+export async function loadConfig(): Promise<Config> {
+  let config = await getConfigDefault();
+  if (await exists(CONF_FILENAME, CONF_OPTION)) {
+    const contents = await readTextFile(CONF_FILENAME, CONF_OPTION);
+    config = JSON.parse(contents) as Config;
+  } else {
+    await writeConfig(config);
+  }
+  return config;
 }
 
 export async function writeConfig(config: Config): Promise<void> {
+  const configDir = await appConfigDir();
+  try {
+    await readDir(configDir);
+  } catch (e) {
+    try {
+      await createDir(configDir, { recursive: true });
+    } catch (e) {
+      throw e;
+    }
+  }
   await writeTextFile(
     CONF_FILENAME,
     JSON.stringify(config, null, "\t"),
