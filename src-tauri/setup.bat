743ffea809e981ed61c7369ff85049c189b9b1fe
miyabi-satoh <miyabi.satoh@gmail.com>
2022-12-25 01:39:17 +0900

    出力内容をいじった

--- src-tauri/setup.bat
+++ src-tauri/setup.bat
@@ -2,11 +2,10 @@
 
 call .venv\Scripts\activate.bat
 if %errorlevel% neq 0 (
-    echo "venvの有効化に失敗しました"
+    echo venvの有効化に失敗しました
     exit /b 1
 ) else (
     python -V
-    echo "venv OK"
 )
 
 python -m pip install --upgrade pip > nul
@@ -14,44 +13,41 @@ python -m pip list > pip.list
 
 findstr "wheel" pip.list > nul
 if %errorlevel% neq 0 (
-    echo "wheelパッケージをインストールします"
-    python -m pip install wheel > nul
+    echo wheelパッケージをインストールします
+    python -m pip install wheel
+    @REM python -m pip list > pip.list
+    @REM findstr "wheel" pip.list > nul
     if %errorlevel% neq 0 (
-        echo "wheelパッケージのインストールに失敗しました"
+        echo wheelパッケージのインストールに失敗しました
         exit /b 1
-    ) else (
-        echo "成功"
     )
-) else (
-    findstr "wheel" pip.list
 )
+findstr "wheel" pip.list
 
 findstr "PyPDF2" pip.list > nul
 if %errorlevel% neq 0 (
-    echo "PyPDF2パッケージをインストールします"
-    python -m pip install 'PyPDF2>=2,<3' > nul
+    echo PyPDF2パッケージをインストールします
+    python -m pip install PyPDF2==2.12.1
+    @REM python -m pip list > pip.list
+    @REM findstr "PyPDF2" pip.list > nul
     if %errorlevel% neq 0 (
-        echo "PyPDF2パッケージのインストールに失敗しました"
+        echo PyPDF2パッケージのインストールに失敗しました
         exit /b 1
-    ) else (
-        echo "成功"
     )
-) else (
-    findstr "PyPDF2" pip.list
 )
+findstr "PyPDF2" pip.list
 
 findstr "pycryptodome" pip.list > nul
 if %errorlevel% neq 0 (
-    echo "pycryptodomeパッケージをインストールします"
-    python -m pip install pycryptodome > nul
+    echo pycryptodomeパッケージをインストールします
+    python -m pip install pycryptodome
+    @REM python -m pip list > pip.list
+    @REM findstr "pycryptodome" pip.list > nul
     if %errorlevel% neq 0 (
-        echo "pycryptodomeパッケージのインストールに失敗しました"
+        echo pycryptodomeパッケージのインストールに失敗しました
         exit /b 1
-    ) else (
-        echo "成功"
     )
-) else {
-    findstr "pycryptodome" pip.list
-}
+)
+findstr "pycryptodome" pip.list
 
 exit /b 0
