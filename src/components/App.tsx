69c510dbf2405d3d2f1c3c00a7196505b30e00a6
miyabi-satoh <miyabi.satoh@gmail.com>
2022-12-26 01:32:26 +0900

    UIをゴリっと変更

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 import {
   appDataDir,
   BaseDirectory,
@@ -17,7 +17,6 @@ import { Config, loadConfig, removeConfig } from "~/utils/config";
 
 const WINDOWS = navigator.userAgent.includes("Windows");
 const SCRIPT_EXT = WINDOWS ? "bat" : "sh";
-const START = WINDOWS ? "start" : "open";
 const WHERE = WINDOWS ? "where" : "which";
 
 const COLOR_ERR = "text-red-600";
@@ -28,112 +27,15 @@ type OpenDirection = "left" | "right";
 export const App = () => {
   const config = useRef<Config>({} as Config);
   const didEffect = useRef(false);
+
   const { addMessage } = useMessageContext();
+
   const [inputPdf, setInputPdf] = useState("");
-  const [savedFile, setSavedFile] = useState("");
+  const [outputPdf, setOutputPdf] = useState("");
   const [openDirection, setOpenDirection] = useState<OpenDirection>("left");
+  const [checkOpen, setCheckOpen] = useState(true);
   const [overlayShows, setOverlayShows] = useState<OverlayShowType>("spinner");
   const [errorContent, setErrorContent] = useState<ReactNode>(null);
-
-  async function selectInputPdf() {
-    const selected = await open({
-      multiple: false,
-      filters: [
-        {
-          name: "PDF Document",
-          extensions: ["pdf"],
-        },
-      ],
-    });
-    if (typeof selected === "string") {
-      setInputPdf(selected);
-      setSavedFile("");
-    }
-  }
-
-  const handleChangeInputPdf: React.ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    setInputPdf(event.target.value);
-  };
-
-  const handleChangeOpenDirection: React.ChangeEventHandler<
-    HTMLInputElement
-  > = (event) => {
-    setOpenDirection(event.target.value as OpenDirection);
-  };
-
-  async function doSaddleStitch() {
-    const name = await basename(inputPdf);
-    const newName = `(製本版)${name}`;
-
-    const savePath = await save({
-      defaultPath: newName,
-      filters: [{ name: "PDF Document", extensions: ["pdf"] }],
-    });
-    if (savePath === null) {
-      return;
-    }
-
-    setOverlayShows("spinner");
-
-    const dataDir = await appDataDir();
-    const scriptPath = await resolveResource(`saddlestitch.${SCRIPT_EXT}`);
-    const command = newCommand(
-      [scriptPath, inputPdf, savePath, openDirection],
-      {
-        cwd: dataDir,
-      }
-    ).on("close", (data) => {
-      setOverlayShows("none");
-      if (data.code === 0) {
-        addMessage(`${savePath} に保存しました`);
-        addMessage("Ready");
-        setSavedFile(savePath);
-      } else {
-        addMessage(`処理はコード${data.code}で失敗しました`, COLOR_ERR);
-      }
-    });
-    command.stdout.on("data", (line) => addMessage(line));
-    command.stderr.on("data", (line) => addMessage(line, COLOR_ERR));
-
-    addMessage("処理を開始します");
-    command.spawn();
-  }
-
-  async function handleRelaunch() {
-    await relaunch();
-  }
-
-  async function handleReset() {
-    const confirmed = await confirm(
-      "設定をリセットして再起動します。\r\nよろしいですか？",
-      { type: "warning" }
-    );
-    if (confirmed) {
-      try {
-        await removeConfig();
-      } catch (e) {
-        debug(e);
-        console.log(e);
-        addMessage("設定ディレクトリの削除に失敗しました", COLOR_ERR);
-        return;
-      }
-      const dataDir = await appDataDir();
-      if ((await dirExists(dataDir)) === true) {
-        try {
-          await removeDir("", { dir: BaseDirectory.AppData, recursive: true });
-        } catch (e) {
-          debug(e);
-          console.log(e);
-          addMessage("データディレクトリの削除に失敗しました", COLOR_ERR);
-          return;
-        }
-      }
-      // 再起動
-      await relaunch();
-    }
-  }
 
   function newCommand(args: string[], options?: SpawnOptions) {
     const CMD = WINDOWS ? "cmd" : "sh";
@@ -154,24 +56,6 @@ export const App = () => {
       encoding: ENC,
       ...options,
     });
-  }
-
-  async function handleOpenFile() {
-    // const command = newCommand([START, `"${savedFile}"`]).on(
-    //   "close",
-    //   (data) => {
-    //     if (data.code !== 0) {
-    //       addMessage(`処理はコード${data.code}で失敗しました`, COLOR_ERR);
-    //     }
-    //   }
-    // );
-    // command.stdout.on("data", (line) => addMessage(line));
-    // command.stderr.on("data", (line) => addMessage(line, COLOR_ERR));
-
-    // command.spawn();
-    const scriptPath = await resolveResource(`openpdf.${SCRIPT_EXT}`);
-    const output = await newCommand([scriptPath, savedFile]).execute();
-    debug(output);
   }
 
   function debug(output: string | ChildProcess) {
@@ -286,6 +170,131 @@ export const App = () => {
     });
   }
 
+  const handleSelectInput = async () => {
+    const selected = await open({
+      multiple: false,
+      filters: [
+        {
+          name: "PDF Document",
+          extensions: ["pdf"],
+        },
+      ],
+    });
+    if (typeof selected === "string") {
+      setInputPdf(selected);
+    }
+  };
+
+  const handleSelectOutput = async () => {
+    const name = await basename(inputPdf);
+    const newName = `(製本版)${name}`;
+
+    const selected = await save({
+      defaultPath: newName,
+      filters: [{ name: "PDF Document", extensions: ["pdf"] }],
+    });
+    if (typeof selected === "string") {
+      setOutputPdf(selected);
+    }
+  };
+
+  const handleSaddleStitch = async () => {
+    setOverlayShows("spinner");
+
+    const dataDir = await appDataDir();
+    const scriptPath = await resolveResource(`saddlestitch.${SCRIPT_EXT}`);
+    const command = newCommand(
+      [scriptPath, inputPdf, outputPdf, openDirection],
+      {
+        cwd: dataDir,
+      }
+    ).on("close", async (data) => {
+      setOverlayShows("none");
+      if (data.code === 0) {
+        addMessage(`${outputPdf} に保存しました`);
+        if (checkOpen) {
+          const scriptPath = await resolveResource(`openpdf.${SCRIPT_EXT}`);
+          const output = await newCommand([scriptPath, outputPdf]).execute();
+          debug(output);
+        }
+        addMessage("Ready");
+      } else {
+        addMessage(`処理はコード${data.code}で失敗しました`, COLOR_ERR);
+      }
+    });
+    command.stdout.on("data", (line) => addMessage(line));
+    command.stderr.on("data", (line) => addMessage(line, COLOR_ERR));
+
+    addMessage("処理を開始します");
+    command.spawn();
+  };
+
+  const handleRelaunch = useCallback(async () => {
+    await relaunch();
+  }, []);
+
+  const handleReset = useCallback(async () => {
+    const confirmed = await confirm(
+      "設定をリセットして再起動します。\r\nよろしいですか？",
+      { type: "warning" }
+    );
+    if (confirmed) {
+      try {
+        await removeConfig();
+      } catch (e) {
+        debug(e);
+        console.log(e);
+        addMessage("設定ディレクトリの削除に失敗しました", COLOR_ERR);
+        return;
+      }
+      const dataDir = await appDataDir();
+      if ((await dirExists(dataDir)) === true) {
+        try {
+          await removeDir("", { dir: BaseDirectory.AppData, recursive: true });
+        } catch (e) {
+          debug(e);
+          console.log(e);
+          addMessage("データディレクトリの削除に失敗しました", COLOR_ERR);
+          return;
+        }
+      }
+      // 再起動
+      await relaunch();
+    }
+  }, []);
+
+  const handleChangeInput: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (event) => {
+        setInputPdf(event.target.value);
+      },
+      [setInputPdf]
+    );
+
+  const handleChangeOutput: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (event) => {
+        setOutputPdf(event.target.value);
+      },
+      [setOutputPdf]
+    );
+
+  const handleChangeOpenDirection: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (event) => {
+        setOpenDirection(event.target.value as OpenDirection);
+      },
+      [setOpenDirection]
+    );
+
+  const handleChangeCheckOpen: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (event) => {
+        setCheckOpen((current) => !current);
+      },
+      [setCheckOpen]
+    );
+
   useEffect(() => {
     const f = async () => {
       // スピナー表示
@@ -301,6 +310,7 @@ export const App = () => {
       doSetup().catch((err) => setOverlayShows("children"));
     };
 
+    // StrictModeでuseEffectが2回呼ばれることへの対策
     if (!didEffect.current) {
       f();
     }
@@ -330,72 +340,105 @@ export const App = () => {
       <div className="py-2 px-4 h-full flex flex-col">
         <div className="py-2 flex flex-col">
           <label htmlFor="input-pdf" className="form-label">
-            変換するPDFファイル
+            変換元ファイルパス
           </label>
-          <input
-            type="text"
-            name="input-pdf"
-            className="form-control"
-            value={inputPdf}
-            onChange={handleChangeInputPdf}
-          />
-          <div className="mt-2 flex items-center">
+          <div className="flex items-center">
+            <input
+              type="text"
+              name="input-pdf"
+              className="form-control flex-1"
+              value={inputPdf}
+              onChange={handleChangeInput}
+            />
             <button
-              className="btn btn-primary mr-8"
+              className="btn btn-primary ml-2"
               type="button"
-              onClick={selectInputPdf}
+              onClick={handleSelectInput}
             >
               選択
             </button>
-
-            <input
-              className="form-check-input"
-              type="radio"
-              value="left"
-              name="direction"
-              id="openLeft"
-              onChange={handleChangeOpenDirection}
-              checked={"left" == openDirection}
-            />
-            <label className="form-check-label" htmlFor="openLeft">
-              左開き
-            </label>
-
-            <input
-              className="form-check-input ml-4"
-              type="radio"
-              value="right"
-              name="direction"
-              id="openRight"
-              onChange={handleChangeOpenDirection}
-              checked={"right" == openDirection}
-            />
-            <label className="form-check-label" htmlFor="openRight">
-              右開き
-            </label>
           </div>
         </div>
+        <div className="py-2 flex flex-col">
+          <label htmlFor="output-pdf" className="form-label">
+            変換先ファイルパス
+          </label>
+          <div className="flex items-center">
+            <input
+              type="text"
+              name="output-pdf"
+              className="form-control flex-1"
+              value={outputPdf}
+              onChange={handleChangeOutput}
+            />
+            <button
+              className="btn btn-primary ml-2"
+              type="button"
+              onClick={handleSelectOutput}
+            >
+              選択
+            </button>
+          </div>
+        </div>
+        <div className="pt-2 flex flex-col">
+          <fieldset>
+            <legend>オプション</legend>
+            <div className="flex items-center">
+              <input
+                className="form-check-input"
+                type="radio"
+                value="left"
+                name="direction"
+                id="openLeft"
+                onChange={handleChangeOpenDirection}
+                checked={"left" == openDirection}
+              />
+              <label className="form-check-label" htmlFor="openLeft">
+                左開き
+              </label>
 
-        <div className="py-8 flex flex-col">
+              <input
+                className="form-check-input ml-4"
+                type="radio"
+                value="right"
+                name="direction"
+                id="openRight"
+                onChange={handleChangeOpenDirection}
+                checked={"right" == openDirection}
+              />
+              <label className="form-check-label" htmlFor="openRight">
+                右開き
+              </label>
+              <input
+                className="form-check-input ml-4"
+                type="checkbox"
+                value={1}
+                onChange={handleChangeCheckOpen}
+                checked={checkOpen}
+                id="checkOpen"
+              />
+              <label className="form-check-label" htmlFor="checkOpen">
+                変換後にファイルを開く
+              </label>
+            </div>
+          </fieldset>
+        </div>
+
+        <div className="flex flex-col">
           <div className="flex justify-center">
             <button
               className="btn btn-primary"
               type="button"
-              onClick={doSaddleStitch}
-              disabled={inputPdf.length == 0}
+              onClick={handleSaddleStitch}
+              disabled={inputPdf.length == 0 || outputPdf.length == 0}
             >
               製本レイアウトに変換
             </button>
           </div>
-          {savedFile.length > 0 && (
-            <div className="flex justify-center mt-2">
-              <a href="#" onClick={handleOpenFile}>
-                ファイルを開く
-              </a>
-            </div>
-          )}
         </div>
+
         <div className="flex-1" />
+
         <div className="flex">
           <button
             className="btn btn-danger btn-sm"
